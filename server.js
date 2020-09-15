a62e5a65ceebb25d86d66a434eaf284c4821668a
DeepshikaS <ddeepu13@gmail.com>
2020-09-15 23:38:01 +0530

    routes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 
 const app = express();
+const users = require("./routes/api/users");
 
 app.use(
   bodyParser.urlencoded({
@@ -18,6 +19,10 @@ mongoose
   .connect(dbKeys, { useNewUrlParser: true })
   .then(() => console.log("mongodb connected successfully"))
   .catch((err) => console.log(err));
+
+app.use("/api/users", users);
+//app.use(app.router);
+//users.initialize(app);
 
 const port = 5000;
 app.listen(port, () => console.log("server is up and running"));
