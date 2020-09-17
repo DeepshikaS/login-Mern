a7312a0074e983a8d4d39ed1a6825c6d6bb923c3
DeepshikaS <ddeepu13@gmail.com>
2020-09-17 23:36:41 +0530

    front end

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from "./layout/Navbar";
 import Landing from "./layout/Landing";
+import Register from "./auth/Register";
+import Login from "./auth/Login";
+import { Provider } from "react-redux";
+import store from "./store";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Navbar />
-        <Landing />
-      </div>
+      <Provider store={store}>
+        <Router>
+          <div className="App">
+            <Navbar />
+            <Route exact path="/" component={Landing} />
+            <Route exact path="/register" component={Register} />
+            <Route exact path="/login" component={Login} />
+          </div>
+        </Router>
+      </Provider>
     );
   }
 }
