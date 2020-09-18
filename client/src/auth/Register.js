a21f5ba055169c50651e0d1eab04307b80f01430
DeepshikaS <ddeepu13@gmail.com>
2020-09-18 22:56:48 +0530

    front end

--- client/src/auth/Register.js
+++ client/src/auth/Register.js
@@ -16,6 +16,13 @@ class Register extends Component {
       errors: {},
     };
   }
+
+  componentDidMount() {
+    if (this.props.auth.isAuthenticated) {
+      this.props.history.push("/dashboard");
+    }
+  }
+
   componentWillReceiveProps(nextProps) {
     if (nextProps.errors) {
       this.setState({
